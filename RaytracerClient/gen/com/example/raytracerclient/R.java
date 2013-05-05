cf1517e565029d2057726d299c2ecb7dccf70f9c
favreau <cyrille.favreau@gmail.com>
2013-05-05 17:46:12 +0000

    Android
    git-svn-id: https://beast-pc/svn/GPGPU/Android/RaytracerClient/trunk@711 c4de0f11-4f5c-764a-a4aa-d9e147179871

--- RaytracerClient/gen/com/example/raytracerclient/R.java
+++ RaytracerClient/gen/com/example/raytracerclient/R.java
@@ -25,17 +25,23 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int dummy_button=0x7f070002;
+        public static final int LinearLayout=0x7f070001;
+        public static final int btnRender=0x7f070003;
+        public static final int etModel=0x7f070005;
         public static final int etURL=0x7f070004;
         public static final int fullscreen_content=0x7f070000;
-        public static final int fullscreen_content_controls=0x7f070001;
-        public static final int ivRendering=0x7f070003;
+        public static final int fullscreen_content_controls=0x7f070002;
+        public static final int ivRendering=0x7f070006;
     }
     public static final class layout {
         public static final int activity_fullscreen=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int default_model=0x7f050004;
+        /** <string name="default_url">http://192.168.1.3:10000</string> 
+         */
+        public static final int default_url=0x7f050003;
         public static final int dummy_button=0x7f050001;
         public static final int dummy_content=0x7f050002;
     }
--- RaytracerClient/src/com/example/raytracerclient/FullscreenActivity.java
+++ RaytracerClient/src/com/example/raytracerclient/FullscreenActivity.java
@@ -18,6 +18,7 @@ import android.util.Base64;
 import android.view.MotionEvent;
 import android.view.MotionEvent.PointerCoords;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
@@ -38,180 +39,180 @@ import org.apache.http.protocol.HttpContext;
 public class FullscreenActivity extends Activity {
 
 	private static EditText edURL = null;
+	private static EditText edModel = null;
 	private static ImageView ivRendering = null;
 	private static String imageContent = null;
 	private static boolean renderingInProgress=false;
 	private PointerCoords mousePos = new PointerCoords();
 	private PointerCoords oldMousePos = new PointerCoords();
 	private PointerCoords viewAngles = new PointerCoords();
-	
-	 private class DownloadFilesTask extends AsyncTask<URL, Integer, Long> {
-	     protected Long doInBackground(URL... urls) {
-	    	 renderingInProgress = true;
-     	    HttpClient httpclient = new DefaultHttpClient();
-     	    HttpContext localContext = new BasicHttpContext();
-     	    
-     	    // Prepare a request object
-     	    HttpGet httpget = new HttpGet( edURL.getText().toString() + 
-     	    		"/get?molecule=1SUI&scheme=0&postprocessing=0&bkcolor=255,255,255&structure=0&size=0&quality=1" +
-     	    		"&rotation=" + viewAngles.x + "," + viewAngles.y + ",0&scene=0&distance=-5000&depth=50000&fake=0&values=0,0");
-     	     
-     	    try {
-         	    // Execute the request
-     	    	HttpResponse response = httpclient.execute(httpget, localContext);
-     	    	
-     	        // Get hold of the response entity
-     	        HttpEntity entity = response.getEntity();
-     	        
-     	        imageContent = getASCIIContentFromEntity(entity);
-     	       
-     	        
-     	    } 
-     	    catch (Exception e) {
-     	    	ivRendering.setBackgroundColor(Color.RED);
-     	    }
+
+	private class DownloadFilesTask extends AsyncTask<URL, Integer, Long> {
+		protected Long doInBackground(URL... urls) {
+			renderingInProgress = true;
+			
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpContext localContext = new BasicHttpContext();
+			
+			// Prepare a request object
+			HttpGet httpget = new HttpGet( edURL.getText().toString() + 
+					"/get?irt=" + edModel.getText().toString() + 
+					"&postprocessing=0&bkcolor=255,255,255&size=0&quality=1" +
+					"&rotation=" + viewAngles.x + "," + viewAngles.y + ",0&scene=0&distance=-10000&depth=50000&fake=0&values=0,0");
+
+			try {
+				// Execute the request
+				HttpResponse response = httpclient.execute(httpget, localContext);
+
+				// Get hold of the response entity
+				HttpEntity entity = response.getEntity();
+
+				imageContent = getASCIIContentFromEntity(entity);
+
+
+			} 
+			catch (Exception e) {
+				ivRendering.setBackgroundColor(Color.RED);
+			}
 			return null;        		
-	     }
+		}
 
-	     protected void onProgressUpdate(Integer... progress) {
-	     }
+		protected void onProgressUpdate(Integer... progress) {
+		}
 
-	     protected void onPostExecute(Long result) {
-	    	 imageContent = imageContent.substring(21);
-	    	 byte[] b = Base64.decode(imageContent, Base64.DEFAULT);
-	    	 Bitmap decodedByte = BitmapFactory.decodeByteArray(b, 0, b.length);
-	    	 ivRendering.setImageBitmap(decodedByte);
-	    	 renderingInProgress = false;
-	     }
+		protected void onPostExecute(Long result) {
+			imageContent = imageContent.substring(21);
+			byte[] b = Base64.decode(imageContent, Base64.DEFAULT);
+			Bitmap decodedByte = BitmapFactory.decodeByteArray(b, 0, b.length);
+			ivRendering.setImageBitmap(decodedByte);
+			renderingInProgress = false;
+		}
 
-	 }	
+	}	
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_fullscreen);
+		setContentView(R.layout.activity_fullscreen);
 
-        
-        edURL     = (EditText)findViewById(R.id.etURL);
- 	    ivRendering  =(ImageView)findViewById(R.id.ivRendering);
- 	   mousePos.x = ivRendering.getWidth()/2;
- 	   mousePos.y = ivRendering.getHeight()/2;
- 	   oldMousePos.x = ivRendering.getWidth()/2;
- 	   oldMousePos.y = ivRendering.getHeight()/2;
-  	  viewAngles.x = 0;
- 	  viewAngles.y = 0;
- 		
+		edURL     = (EditText)findViewById(R.id.etURL);
+		edModel   = (EditText)findViewById(R.id.etModel);
+		ivRendering  =(ImageView)findViewById(R.id.ivRendering);
+		
+		mousePos.x = ivRendering.getWidth()/2;
+		mousePos.y = ivRendering.getHeight()/2;
+		oldMousePos.x = ivRendering.getWidth()/2;
+		oldMousePos.y = ivRendering.getHeight()/2;
+		viewAngles.x = 20;
+		viewAngles.y = 20;
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
-        ivRendering.setOnTouchListener(mChangeRotation);
-    }
+		// Upon interacting with UI controls, delay any scheduled hide()
+		// operations to prevent the jarring behavior of controls going away
+		// while interacting with the UI.
+		findViewById(R.id.btnRender).setOnTouchListener(mDelayHideTouchListener);
+		ivRendering.setOnTouchListener(mChangeRotation);
+	}
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
 
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
-    }
+		// Trigger the initial hide() shortly after the activity has been
+		// created, to briefly hint to the user that UI controls
+		// are available.
+		delayedHide(100);
+	}
 
-    protected String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
-    	InputStream in = entity.getContent();
-    	StringBuffer out = new StringBuffer();
-    	int n = 1;
-    	while (n>0) {
-    		byte[] b = new byte[4096];
-    		n =  in.read(b);
-    		if (n>0) out.append(new String(b, 0, n));
-    	}
-    	return out.toString();
-    }
+	protected String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
+		InputStream in = entity.getContent();
+		StringBuffer out = new StringBuffer();
+		int n = 1;
+		while (n>0) {
+			byte[] b = new byte[4096];
+			n =  in.read(b);
+			if (n>0) out.append(new String(b, 0, n));
+		}
+		return out.toString();
+	}
 
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-        	if( !renderingInProgress )
-        	{
-        		viewAngles.x = 20;
-        		viewAngles.y = 0;
-	        	if( motionEvent.getAction() == MotionEvent.ACTION_DOWN )
-	        	{
-	        		DownloadFilesTask task = new DownloadFilesTask();
-	        		URL url;
+	/**
+	 * Touch listener to use for in-layout UI controls to delay hiding the
+	 * system UI. This is to prevent the jarring behavior of controls going away
+	 * while interacting with activity UI.
+	 */
+	View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+		@Override
+		public boolean onTouch(View view, MotionEvent motionEvent) {
+			if( !renderingInProgress )
+			{
+				viewAngles.x = 20;
+				viewAngles.y = 0;
+				if( motionEvent.getAction() == MotionEvent.ACTION_DOWN )
+				{
+					DownloadFilesTask task = new DownloadFilesTask();
+					URL url;
 					try {
-						url = new URL("http://192.168.1.3:10000/get?"); // To remove
-		        		task.execute(url);
+						url = new URL(edURL.getText().toString()); // To remove
+						task.execute(url);
 					} catch (MalformedURLException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-	        	}
-        	}
-            return false;
-        }
-    };
+				}
+			}
+			return false;
+		}
+	};
 
-    View.OnTouchListener mChangeRotation = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent dragEvent) {
-        	//if( dragEvent.getAction()==MotionEvent.ACTION_MOVE )
-        	{
-	    		mousePos.x = dragEvent.getX();
-	    		mousePos.y = dragEvent.getY();
-	    		float value = (oldMousePos.x-mousePos.x)/100;
-	    		if( Math.abs(value)<1 )
-	    		{
-	    			viewAngles.y += 20*Math.asin(value);  
-	    		}
-	    		value = (oldMousePos.y-mousePos.y)/100;
-	    		if( Math.abs(value)<1 )
-	    		{
-	    			viewAngles.x += 20*Math.asin(value);  
-	    		}
-		    	if( !renderingInProgress ) 
-		    	{
-		    		DownloadFilesTask task = new DownloadFilesTask();
-		    		URL url;
+	View.OnTouchListener mChangeRotation = new View.OnTouchListener() {
+		@Override
+		public boolean onTouch(View view, MotionEvent dragEvent) {
+			//if( dragEvent.getAction()==MotionEvent.ACTION_MOVE )
+			{
+				mousePos.x = dragEvent.getX();
+				mousePos.y = dragEvent.getY();
+				float value = (oldMousePos.x-mousePos.x)/100;
+				if( Math.abs(value)<1 )
+				{
+					viewAngles.y += 20*Math.asin(value);  
+				}
+				value = (oldMousePos.y-mousePos.y)/100;
+				if( Math.abs(value)<1 )
+				{
+					viewAngles.x += 20*Math.asin(value);  
+				}
+				if( !renderingInProgress ) 
+				{
+					DownloadFilesTask task = new DownloadFilesTask();
+					URL url;
 					try {
-						url = new URL("http://192.168.1.3:10000/get?"); // To remove
-		        		task.execute(url);
+						url = new URL(edURL.getText().toString()); // To remove
+						task.execute(url);
 					} catch (MalformedURLException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-		    	}
-		    	
-		    	oldMousePos.x = mousePos.x;
-	    		oldMousePos.y = mousePos.y;
-        	}
-    		return false;
-        }
-    };
+				}
 
-    Handler mHideHandler = new Handler();
-    Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            //mSystemUiHider.hide();
-        }
-    };
+				oldMousePos.x = mousePos.x;
+				oldMousePos.y = mousePos.y;
+			}
+			return false;
+		}
+	};
 
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        //mHideHandler.removeCallbacks(mHideRunnable);
-        //mHideHandler.postDelayed(mHideRunnable, delayMillis);
-    }
+	Handler mHideHandler = new Handler();
+	Runnable mHideRunnable = new Runnable() {
+		@Override
+		public void run() {
+		}
+	};
+
+	/**
+	 * Schedules a call to hide() in [delay] milliseconds, canceling any
+	 * previously scheduled calls.
+	 */
+	private void delayedHide(int delayMillis) {
+	}
 }
